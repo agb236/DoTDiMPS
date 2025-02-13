de847297400fbbea87ecdd0690a584bd484d25fe
Adam <agb236@gmail.com>
2025-02-13 15:25:15 +0100

    CookieCutter Template

---
+++ Detection of topological differences in multimer protein structures/src/dotdimps/train.py
@@ -0,0 +1,10 @@
+from dotdimps.model import Model
+from dotdimps.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
