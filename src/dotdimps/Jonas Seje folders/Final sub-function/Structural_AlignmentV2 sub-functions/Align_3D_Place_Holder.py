35986369d577dbe8af7eab4873876465a39dbc43
Adam <agb236@gmail.com>
2025-03-13 15:36:11 +0100

    CandC part 4

---
+++ src/dotdimps/Jonas Seje folders/Final sub-function/Structural_AlignmentV2 sub-functions/Align_3D_Place_Holder.py
@@ -0,0 +1,58 @@
+from numpy import array, linalg, linspace, transpose, zeros, mean, ndarray, sign, sqrt, sum
+from numpy.linalg import eig, svd, lstsq, det
+
+
+def Align_3D(P1, P2):
+    """
+    Aligns two sets of 3D points using the Kabsch algorithm.
+    The points in P2 are rotated to minimize the RMSD to the points in P1. P1 and P2 should both
+    be centered at the origin.
+    :param P1: ndarray
+    :param P2: ndarray
+    :return: ndarray
+    """
+    assert isinstance(P1, ndarray) and isinstance(P2, ndarray), "P1 and P2 must be ndarrays"
+    assert P1.shape == P2.shape, "P1 and P2 must have the same shape"
+    n = P1.shape[0]
+    meanp = mean(P2,axis=0)
+    meanpt = mean(P1, axis = 0)
+    #print(meanp, meanpt)
+
+    q= P2-meanp
+    qt = P1-meanpt
+
+    Qt = transpose(qt)
+    Q = transpose(q)
+
+
+    A = Qt@transpose(Q)
+    u, s, vh = svd(A, full_matrices=True)
+    d = sign(det(u@vh))
+
+    R = u@array([[1,0,0],[0,1,0],[0,0,d]])@(vh)
+
+    # t = 1/n*(P1.sum(axis = 0)-P2.sum(axis=0))
+    # print(t)
+    transformed_pts = transpose(R@transpose(q))+meanp
+
+    RMSD = sqrt(1/n * linalg.norm(qt-transformed_pts,"fro")**2) #frobenius norm
+    # RMSD = sqrt(sum((P1 - transformed_pts)**2) / n)
+
+    return transformed_pts, R, RMSD
+
+
+# #generate test data
+# P1 = array([[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
+# P2 = array([[0,0,0],[1,0,0],[0,1,0],[0,0,1]]) + 0.1
+
+
+# transformed_pts, RMSD = Align_3D(P1, P2)
+
+# #plot the data, using plotly
+# import plotly.graph_objects as go
+
+# fig = go.Figure()
+
+# fig.add_trace(go.Scatter3d(x=P1[:,0], y=P1[:,1], z=P1[:,2], mode='markers', marker=dict(size=12, color='blue')))
+# fig.add_trace(go.Scatter3d(x=transformed_pts[:,0], y=transformed_pts[:,1], z=transformed_pts[:,2], mode='markers', marker=dict(size=12, color='green')))
+# fig.show()
